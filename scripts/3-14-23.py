eb8e23edb1beda04a60b6085307fd2caee0bb3d0
messamat <mathis.messager@mail.mcgill.ca>
2023-03-15 17:06:21 +0100

    3-4-23.py: Speed up functions (reduce number of regex calls). Refine wildcards.

--- scripts/3-14-23.py
+++ scripts/3-14-23.py
@@ -1,5 +1,6 @@
+#import cProfile
 import re
-import itertools as it
+#import itertools as it
 
 # def findNear(text,word1,word2,precede=False):
 #     wa = re.findall(rf"{word1}" +r"\W+(?:\w+\W+){0,0}?" + rf"{word2}" + r"\b",prepped_text)
@@ -13,20 +14,20 @@ def findSimp(text,word1,word2,precede=False):
     '''
     precede = False means word1 and word2 are looked at with either being first word
     precede = True means word1 must be first, word2 must be second
-    
     '''
 
-    wa = re.findall(rf"{word1}" +r"\W" + rf"{word2}" + r"\b",prepped_text)
-    wal = len(wa)
+    regexp = f"{word1}\\W{word2}\\b"
     if precede==False:
-        wa1 = re.findall(rf"{word2}" +r"\W" + rf"{word1}" + r"\b",prepped_text)
-        wal1 = len(wa1)
-        return wal + wal1
-    else:
-        return wal
+        regexp = f"({regexp})|({word2}\\W{word1}\\b)"
+
+    wa = re.findall(regexp, text)
+    wal = len(wa)
+
+    return(wal)
+
 
 def findSuperSimp(text,word):
-    return len(re.findall(rf"{word}",text))
+    return len(re.findall(f"{word}", text))
 
 
 text0 = ['''
@@ -65,8 +66,8 @@ prepped_text = text0[0].replace("\n","")
 
 search queries
 
-\S* means any non whitespace character repeated an one or more number of times
-\S means any single non whitespace character
+\S* matches any non whitespace character repeated zero or more times
+\S? means any single non whitespace character, zero or one time
 \S\S means any two non whitespace characters, etc...
 
 https://www.py4e.com/lessons/regex
@@ -74,63 +75,78 @@ https://www.py4e.com/lessons/regex
 '''
 
 combo1_1 = ["ecologic\S*",'environmental\S*', 'minim\S\S', 'acceptable',
-'augment\S*', 'compensation', 'experimental', 'flushing', 
-'in-stream', 'instream', 'maintenance', 'optimum', 'restor\S*']
+            'augment\S*', 'compensation', 'experimental', 'flushing',
+            'in[-]?stream', 'maintenance', 'optimum', 'restor\S*']
 
-combo1_2 = ["flood\S*", 'flow\S*', 'freshwater\S','water', 
-'water level\S', 'discharge']
+combo1_2 = ["flood\S*", 'flow\S*', '\S*water\S*',
+            'water level\S?', 'discharge']
 
-combo2_1 = ['conservation','cultural','cut-off','cutoff','design','fish','functional',
-'management','maximum','natural','preference','protection','rating','regime','residual',
-'right\S','sanita\S*','scenario','standard','suitable','surplus','sustainable','threshold',
-'use','vital']
-combo2_2 = ['flow\S']
+combo2_1 = ['conservation', 'cultural', 'cut[-]?off', 'design', 'fish', 'functional',
+            'management', 'maximum', 'natural', 'preference', 'protection', 'rating', 'regime', 'residual',
+            'right\S?', 'sanita\S*', 'scenario', 'standard', 'suitable', 'surplus', 'sustainable', 'threshold',
+            'use', 'vital']
+combo2_2 = ['flow\S?']
 
-combo3_1 = ['downstream','dam','reservoir']
-combo3_2 = ['water release\S','flow release\S']
+combo3_1 = ['downstream', 'dam', 'reservoir']
+combo3_2 = ['water release\S?', 'flow release\S?']
 
-combo4_1 = ['controlled','artificial']
-combo4_2 = ['flood']
+combo4_1 = ['controlled', 'artificial']
+combo4_2 = ['flood\S?']
 
 combo5_1 = ['hydrologic\S*']
-combo5_2 = ['requirement','manipulation\S']
+combo5_2 = ['requirement\S?', 'manipulation\S?']
 
-combo6_1 = ['\S*flow\S','freshwater\S','\S*water\S*','water level\S']
-combo6_2 = ['abstraction','allocation','criteri\S*','delivery','demand\S','guideline\S',
-'need','provision','requirement','reserv\S*','restrict\S*','withdrawal']
+combo6_1 = ['\S*flow\S?', 'freshwater\S?', '\S*water\S*', 'water level\S?']
+combo6_2 = ['abstraction\S?', 'allocation\S?', 'criteri\S*', 'deliver\S*', 'demand\S?', 'guideline\S?',
+            'need\S?', 'provision', 'requirement\S?', 'reserv\S*', 'restrict\S*', 'withdrawal\S?']
+
+#Join all strings in a list with | signs and parentheses
+def recomb(in_str):
+    if isinstance(in_str, list):
+        return(f"({'|'.join(f'({w})' for w in in_str)})")
+    else:
+        return(in_str)
+
+#Profile whether faster to use product upstream or to use | in regex
+# with cProfile.Profile() as prof:
+#     for x in range(100000):
+#         x1 = list(it.product(combo1_1, combo1_2))
+#         for j in x1:
+#             findSimp(prepped_text, j[0], j[1],precede=False)
+# prof.print_stats()
+#
+# with cProfile.Profile() as prof:
+#     x1 = [recomb(combo1_1), recomb(combo1_2)]
+#     for x in range(100000):
+#         findSimp(prepped_text,x1[0],x1[1],precede=False)
+# prof.print_stats()
+###### Using | in regex exp is 6.5 faster
+
+searchdict = {'x1': ['with', [recomb(combo1_1), recomb(combo1_2)]],
+              'x2': ['with', [recomb(combo2_1), recomb(combo2_2)]],
+              'x3': ['pre', [recomb(combo3_1), recomb(combo3_2)]],
+              'x4': ['pre', [recomb(combo4_1), recomb(combo4_2)]],
+              'x5': ['pre', [recomb(combo5_1), recomb(combo5_2)]],
+              'x6': ['with', [recomb(combo6_1), recomb(combo6_2)]],
+              'xeflows': [None, rf'e[-]?flow\S?']
+              }
+
+def combo_refind(in_searchdict, text):
+    for regexp_combo in in_searchdict.values():
+        if regexp_combo[0] == 'with':
+            k = findSimp(text, regexp_combo[1][0], regexp_combo[1][1], precede=False)
+        elif regexp_combo[0] == 'pre':
+            k = findSimp(text, regexp_combo[1][0], regexp_combo[1][1], precede=True)
+        elif regexp_combo[0] is None:
+            k = findSuperSimp(text, regexp_combo)
+        else:
+            break
+
+        return (k)
+        # if k > 0:
+        #
+        #     break
 
 
-x1 = list(it.product(combo1_1,combo1_2))
-x2 = list(it.product(combo2_1,combo2_2))
-x3 = list(it.product(combo3_1,combo3_2))
-x4 = list(it.product(combo4_1,combo4_2))
-x5 = list(it.product(combo5_1,combo5_2))
-x6 = list(it.product(combo6_1,combo6_2))
-xeflows1 = ['e-flow']
-xeflows2 = ['e-flows']
-
-X = [x1,x2,x3,x4,x5,x6,xeflows1,xeflows2]
-for idx,i in enumerate(X):
-    if idx in [0,1,5]:
-        for j in i:
-            k = findSimp(prepped_text,j[0],j[1],precede=False)
-            if k > 0:
-                print(f'found match')
-                break
-        break
-
-    elif idx in [2,3,4]:
-        for j in i:
-            k = findSimp(prepped_text,j[0],j[1],precede=True)
-            if k > 0:
-                print('found match')
-                break
-        break
-
-    elif idx in [6,7]:
-        for j in i:
-            k = findSuperSimp(prepped_text,j)
-            if k > 0:
-                print('found match')
-                break
-        break
+combo_refind(in_searchdict = searchdict,
+             text = prepped_text)
